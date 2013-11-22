fce6450ba5e9c3a28f3631484217a434fd12eb34
Dean Jones <dean@mismatchedreds.com>
2013-11-21 21:40:04 -0700

    added unit tests

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Dean Jones on 11/21/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
