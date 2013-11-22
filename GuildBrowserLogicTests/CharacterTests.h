aa0c39a80ccb8a2012d6b1e10269bdd2ab8d4a8f
Dean Jones <dean@mismatchedreds.com>
2013-11-21 22:21:19 -0700

    more unit tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Dean Jones on 11/21/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
